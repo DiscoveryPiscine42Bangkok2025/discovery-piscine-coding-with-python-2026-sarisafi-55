1310e60377064d92a35927cde158fd62f7864e67
sarisafi-55 <149873381+sarisafi-55@users.noreply.github.com>
2026-02-10 11:54:50 +0700

    Add files via upload

---
+++ cell03/ex03/advanced_mult.py
@@ -0,0 +1,15 @@
+import sys
+if len(sys.argv) > 1:
+    print("none")
+else:
+    table = 0
+    while table <= 10:
+        print(f"Table de {table}:", end=" ")
+
+        multiplier = 0
+        while multiplier <= 10:
+            print(table * multiplier, end=" ")
+            multiplier += 1
+
+        print()
+        table += 1
