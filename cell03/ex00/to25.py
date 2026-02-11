f4df39d71c769ac2e52e06119af9960ad9693ab8
sarisafi-55 <149873381+sarisafi-55@users.noreply.github.com>
2026-02-11 23:29:19 +0700

    Update to25.py

--- cell03/ex00/to25.py
+++ cell03/ex00/to25.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 number = int(input("Enter a number less than 25\n"))
 if number > 25:
     print("Error")
@@ -5,3 +6,4 @@ else:
     while number <= 25:
         print(f"Inside the loop, my variable is {number}")
         number += 1
+
