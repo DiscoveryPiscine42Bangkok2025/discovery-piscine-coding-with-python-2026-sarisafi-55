ad26280dedb24020a16341a87edc7871815de27b
sarisafi-55 <149873381+sarisafi-55@users.noreply.github.com>
2026-02-11 23:31:38 +0700

    Update play_with_arrays.py

--- cell05/ex01/play_with_arrays.py
+++ cell05/ex01/play_with_arrays.py
@@ -1,6 +1,8 @@
+#!/usr/bin/env python3
 original_array = [2, 8, 9, 48, 8, 22, -12, 2]
 new_array = []
 for value in original_array:
     new_array.append(value + 2)
 print("Original array:", original_array)
 print("New array:", new_array)
+
