f9bb09548946936f967cdadf6c3a6b8d40bb6cb7
sarisafi-55 <149873381+sarisafi-55@users.noreply.github.com>
2026-02-11 23:28:33 +0700

    Update isneg.py

--- cell02/ex01/isneg.py
+++ cell02/ex01/isneg.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 number = int(input())
 if number < 0:
     print("This number is negative.")
@@ -5,3 +6,4 @@ elif number > 0:
     print("This number is positive.")
 else:
     print("This number is both positive and negative.")
+
